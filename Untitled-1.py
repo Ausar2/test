46f8d46e5ee987344ead00cb8ac9958619a9fb4a
Ausar2 <93778093+Ausar2@users.noreply.github.com>
2021-11-29 22:31:07 +0700

    Add files via upload

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,17 @@
-print("hello world")
+import io, os
+from google.cloud import vision
+from google.cloud.vision import types
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"ServiceAccToken.json"
+
+client = vision.ImageAnnotatorClient()
+
+FILE_NAME = 'text1.jpg'
+FOLDER_PATH = r'C:\Personal\Miscellaneous\Coding_things\VisionAPIDemo\Images'
+
+with io.open(os.path.join(FOLDER_PATH, FILE_NAME), 'rb') as image_file:
+    content = image_file.read()
+
+image = vision.types.Image(content=content)
+
+response = client.text_detection(image=image)
